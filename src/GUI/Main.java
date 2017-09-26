3b53c64aa4cd644fa100285e592d4e60be1dea85
Nikita Efimuk <32060227+nefimuk@users.noreply.github.com>
2017-09-26 21:37:34 +0300

    update

---
+++ src/GUI/Main.java
@@ -0,0 +1,33 @@
+package GUI;
+
+import javafx.embed.swing.JFXPanel;
+
+import javax.swing.*;
+import java.io.IOException;
+
+public class Main {
+    static JFrame jFrame = getFrame();
+
+    static JPanel jPanel = new JPanel();
+
+    static JFrame getFrame() {
+        JFrame jFrame = new JFrame() {};
+        jFrame.setVisible(true);
+        jFrame.setBounds(750,250,500,500);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        return jFrame;
+    }
+
+    public static void main(String[] args) throws Exception {
+        //Window myWindow = new Window("Nikita");
+        jFrame.add(jPanel);
+        JTextArea jTextArea = new JTextArea(5,20);
+        jTextArea.setLineWrap(true);
+        JScrollPane jScrollPane = new JScrollPane(jTextArea);
+
+        jPanel.add(jScrollPane);
+        jPanel.revalidate();
+
+
+    }
+}
