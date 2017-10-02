7fe7f24b21bc713b20e7cb6b37b77ae8e382c77b
Nikita Efimuk <32060227+nefimuk@users.noreply.github.com>
2017-10-02 20:37:13 +0300

    try

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,6 @@ public class Main {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        System.out.println("Nikita");
+        System.out.println("Nikita just do it");
     }
 }
