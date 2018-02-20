57917836d30830f03c25dd8bb94fe01006310266
Nikita Efimuk <efimukn@gmail.com>
2018-02-20 17:15:32 +0300

    A

---
+++ src/RefValue/RefValue2.java
@@ -0,0 +1,22 @@
+package RefValue;
+
+public class RefValue2 {
+    public static void main(String[] args) {
+        int x = 0;
+        int[] arr = {20};
+        x = f (x, g(x, arr));
+        System.out.println(x + " " +arr[0]);
+    }
+
+    private static int f(int x, int[] arr) {
+        x += 30;
+        arr[0] = 40;
+        return 42;
+    }
+
+    private static int[] g(int x, int[] arr) {
+        x = 50;
+        arr = new int[] {60};
+        return arr;
+    }
+}
