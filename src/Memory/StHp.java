4f0903581782e8ccda577d75e083eca733fbd3e0
Nikita Efimuk <efimukn@gmail.com>
2018-02-21 01:03:52 +0300

    Memory added

---
+++ src/Memory/StHp.java
@@ -0,0 +1,18 @@
+package Memory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StHp {
+
+    public static void main(String[] args) {
+        //long[] arr0 = new long[Integer.MAX_VALUE];
+        //long[] arr1 = new long[999999999];
+
+        List<Object> list = new ArrayList<>();
+        while (true) {
+            list.add(new Object());
+        }
+    }
+
+}
